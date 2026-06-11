cd5c97b862fad825fe4b6051a5f521d122be992a
jtt <jtt@jtt.jtt>
2026-06-11 02:38:02 +0200

    Mise à jour du projet

--- lancer.sh
+++ lancer.sh
@@ -1,10 +1,12 @@
 clear
 echo "Mise à jour local du projet..."
+sleep 2
 git add .
 git commit -m "Mise à jour du projet"
 git push
-
+sleep 2
 clear
+
 echo "Mise à jour du projet sur le serveur..."
 ssh jtt@ssh-jtt.alwaysdata.net -t "~/update.sh"
 clear
