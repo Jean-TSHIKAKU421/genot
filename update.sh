cb5dbfb0bdd09b931fd5d9d1157e8746b6b64b66
jtt <jtt@jtt.jtt>
2026-06-19 23:27:08 +0200

    correction route PUT image_url

--- update.sh
+++ update.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+echo "🔄 Mise à jour de GeNot sur votre machine locale..."
+sleep 2
+clear
 echo "🔄 Mise à jour de GeNot sur GitHub..."
 git add .
 git commit -m "Mise à jour automatique"
 git push
 sleep 2
 clear
+
 
 echo "🔄 Mise à jour de GeNot sur serveur distant..."
 ssh jtt@ssh-jtt.alwaysdata.net "./update.sh"
